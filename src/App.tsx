c5c0c37dfb0e425bd6bcc25dc54b805a391e9914
DESKTOP-60OR2O8\anduser <e.kirichuk@andersenlab.com>
2020-09-14 22:30:35 +0300

    remove extra dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,17 @@ import { Provider } from 'react-redux';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import store, { persistor } from './store/configureStore';
 import { AppRoutes } from './AppRouter';
-import betonTheme from './utils/beton-theme';
+import betonTheme from './utils/yes-theme';
 import { FullscreenLoader } from './components/loader';
-// import './i18n';
 import './App.scss';
 
 function App() {
   return (
     <Provider store={store}>
       <MuiThemeProvider theme={betonTheme}>
-          <React.Suspense fallback={<FullscreenLoader />}>
-            <AppRoutes />
-          </React.Suspense>
+        <React.Suspense fallback={<FullscreenLoader />}>
+          <AppRoutes />
+        </React.Suspense>
       </MuiThemeProvider>
     </Provider>
   );
