7e8a4ba60b3244b17f473e1e09d1398031260737
Evgeny Kirichuk <e.kirichuk@andersenlab.com>
2022-01-05 12:07:00 +0100

    remove extra .git from command

--- src/view/pages/install/Install.tsx
+++ src/view/pages/install/Install.tsx
@@ -12,7 +12,7 @@ const Install = () => {
 	const [isCopied, setIsCopied] = useState(false);
 
 	const command =
-		'npx degit https://github.com/evgeny-kirichuk/yesjs-react-app.git my-app';
+		'npx degit https://github.com/evgeny-kirichuk/yesjs-react-app my-app';
 
 	const copyCommand = () => {
 		navigator.clipboard.writeText(command).then(
